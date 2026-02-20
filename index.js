2057e1c1add328a2332407a42b31d45ec82e60a3
codecraft-sanju <sanjusirvich18@gmail.com>
2026-02-21 02:06:04 +0530

    f

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+const express = require('express');
 const nodemailer = require('nodemailer');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
@@ -12,17 +16,25 @@ const transporter = nodemailer.createTransport({
   debug: true
 });
 
-const mailOptions = {
-  from: 'giftomizeofficial@gmail.com',
-  to: 'sanjaychoudhury693@gmail.com',
-  subject: 'Giftomize Nodemailer Test',
-  text: 'Agar yeh email tumhe mil gaya hai, toh nodemailer bilkul perfect kaam kar raha hai!'
-};
+app.get('/test-email', (req, res) => {
+  const mailOptions = {
+    from: 'giftomizeofficial@gmail.com',
+    to: 'sanjaychoudhury693@gmail.com',
+    subject: 'Giftomize Nodemailer Test Production',
+    text: 'Production server se email aa gaya hai!'
+  };
 
-transporter.sendMail(mailOptions, function(error, info) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      console.log(error);
+      res.status(500).send('Error aayi: ' + error.message);
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.send('Email successfully bhej diya gaya hai!');
+    }
+  });
+});
+
+app.listen(PORT, () => {
+  console.log('Server is running on port ' + PORT);
 });
